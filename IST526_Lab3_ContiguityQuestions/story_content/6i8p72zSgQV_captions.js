41be4c284d442cd8555b13248e444016ff307152
CSUMBNick <ncoonrod@csumb.edu>
2025-03-04 23:27:00 -0800

    lab 3

---
+++ IST526_Lab3_ContiguityQuestions/story_content/6i8p72zSgQV_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.97.34031.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.821%0D%0ANice%20work!%20Click%20to%20complete%20this%20training.%0D%0A%0D%0A"}')
