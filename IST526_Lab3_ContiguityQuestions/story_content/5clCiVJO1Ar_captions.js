28e9263cfb3cb38e60d42896ac978977a0f69f8d
CSUMBNick <ncoonrod@csumb.edu>
2025-03-18 16:22:42 -0700

    vbnnm,g

--- IST526_Lab3_ContiguityQuestions/story_content/5clCiVJO1Ar_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.97.34031.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.560%0D%0AWhich%20best%20describes%20the%20Contiguity%20Principle?%0D%0A%0D%0A"}')
