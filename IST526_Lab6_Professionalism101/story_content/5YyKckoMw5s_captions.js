d6dc8813d3cab17e0add6770850a174eb1209def
CSUMBNick <ncoonrod@csumb.edu>
2025-04-15 16:40:13 -0700

    lab 6

---
+++ IST526_Lab6_Professionalism101/story_content/5YyKckoMw5s_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34380.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.545%0D%0APunctuality%20%E2%80%93%20Be%20on%20time,%20every%20time.%20It%20shows%20respect.%0D%0A%0D%0A"}')
---
+++ IST526_Lab6_Professionalism101/story_content/6XetwurMXmU_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34380.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.403%0D%0ANote%20quite,%20click%20to%20retry%20the%20quix.%0D%0A%0D%0A"}')
