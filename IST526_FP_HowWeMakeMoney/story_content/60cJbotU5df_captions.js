37e954d76661d49fb5c4c3977c5df2c8cefaa7ff
CSUMBNick <ncoonrod@csumb.edu>
2025-06-09 11:06:00 -0700

    rtn

--- IST526_FP_HowWeMakeMoney/story_content/60cJbotU5df_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.98.34380.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.187%0D%0AClick%20to%20learn%20more%20about%20the%20core%20ways%20we%20generate%20revenue.%0D%0A%0D%0A"}')
